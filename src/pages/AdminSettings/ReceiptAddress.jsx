9351a221faa64798cb68a260ae5844c02c9d37e2
devArustu <devendra.arustu@gmail.com>
2025-12-19 12:35:19 +0530

    slvv err

--- src/pages/AdminSettings/ReceiptAddress.jsx
+++ src/pages/AdminSettings/ReceiptAddress.jsx
@@ -1,4 +1,59 @@
+import { useEffect, useState } from "react";
+import { AddDeleteUpdateData, fetchPostData } from "../../components/hooks/Api";
+import toast, { showWarning, showSuccess, showError } from '../../utils/toast';
+
+
 export default function ReceiptAddress() {
+
+    // Company/Update_Company
+    // CompanyID
+    // Address
+
+    const CompanyID = localStorage.getItem('companyID') ?? 1
+    const [Address, setAddress] = useState("");
+
+    useEffect(() => {
+        getPaperImage();
+    }, [CompanyID]);
+
+    const getPaperImage = async () => {
+        const response = await fetchPostData("Company/GetSingleData_Company", {
+            "CompanyID": localStorage.getItem('companyID') || 1,
+        });
+        console.log("🚀 ~ getPaperImage ~ response:", response);
+        if (response?.length > 0) {
+            setAddress(response[0]?.Address);
+
+        } else {
+            setAddress('');
+        }
+    }
+
+    const UpdateContactInfo = (e) => {
+        let error = false;
+
+        if (!Address) {
+            toast.error(`Address is required`);
+            error = true;
+        }
+
+        if (error) {
+            return;
+        }
+        const val = {
+            'Address': Address,
+            'CompanyID': localStorage.getItem('companyID') || 1,
+        }
+        AddDeleteUpdateData('Company/Update_CompanyContact', val).then((response) => {
+            console.log("🚀 ~ handleCheckBox ~ response:", response);
+            if (response?.success) {
+                showSuccess("Update Successfully")
+            }
+        })
+    }
+
+
+
     return (
         <div className="card border">
             <div className="card-body px-3 py-2">
@@ -13,12 +68,15 @@ export default function ReceiptAddress() {
                 {/* Textarea */}
                 <textarea
                     className="form-control mb-2"
-                    rows={3}
                     placeholder=""
+                    name="ButtonDetail"
+                    rows={3}
+                    value={Address}
+                    onChange={(e) => { setAddress(e.target.value) }}
                 />
 
                 {/* Button */}
-                <button className="btn btn-success btn-sm">
+                <button className="btn btn-success btn-sm" onClick={UpdateContactInfo}>
                     UPDATE
                 </button>
 
