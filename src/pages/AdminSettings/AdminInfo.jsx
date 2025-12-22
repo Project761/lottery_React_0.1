a3a8c7f0835dda58349482bf70b94f5fe611f189
devArustu <devendra.arustu@gmail.com>
2025-12-22 13:55:25 +0530

    slv err

--- src/pages/AdminSettings/AdminInfo.jsx
+++ src/pages/AdminSettings/AdminInfo.jsx
@@ -1,4 +1,109 @@
+import { useEffect, useState } from "react";
+import { AddDeleteUpdateData, fetchPostData } from "../../components/hooks/Api";
+import toast, { showWarning, showSuccess, showError } from '../../utils/toast';
+
+
 export default function AdminInfo() {
+
+    const CompanyID = localStorage.getItem('companyID') ?? 1
+
+    const [value, setValue] = useState({
+        "UserID": localStorage.getItem('AdminUserID') || 1,
+        'UserName': '',
+        'EmailID': '',
+        'Password': '',
+
+        // not in use
+        'MobileNo': '',
+        'IsSuperAdmin': '',
+        'ModifiedByUser': '',
+        'UserID': '',
+    });
+
+
+    useEffect(() => {
+        getPaperImage();
+    }, [CompanyID]);
+
+    const getPaperImage = async () => {
+        const response = await fetchPostData("AppUser/GetSingleData_AppUser", {
+            "UserID": localStorage.getItem('AdminUserID') || 1,
+            // "CompanyID": localStorage.getItem('companyID') || 1,
+        });
+        if (response?.length > 0) {
+            setValue({
+                ...value,
+                'UserName': response[0]?.UserName,
+                'EmailID': response[0]?.EmailID,
+                'Password': response[0]?.Password,
+
+                'MobileNo': '',
+                'IsSuperAdmin': '',
+                'ModifiedByUser': '',
+                'UserID': response[0]?.UserID,
+            })
+        } else {
+
+        }
+        console.log("🚀 ~ getPaperImage ~ response:", response);
+    }
+
+
+    const handleSave = async () => {
+        console.log(value);
+
+        let error = false;
+
+        if (!value?.UserName) {
+            toast.error(`ButtonDetail is required`);
+            error = true;
+        }
+
+        if (!value?.EmailID) {
+            toast.error(`TextColor is required`);
+            error = true;
+        }
+
+        if (!value?.Password) {
+            toast.error(`TextColor is required`);
+            error = true;
+        }
+
+        if (error) {
+            return;
+        }
+
+        const { UserName, EmailID, Password } = value
+
+        const val = {
+            // "CompanyID": localStorage.getItem('companyID') || 1,
+            "UserID": localStorage.getItem('AdminUserID') || 1,
+            'UserName': UserName,
+            'EmailID': EmailID,
+            'Password': Password,
+
+            'MobileNo': '',
+            'IsSuperAdmin': '',
+            'ModifiedByUser': '',
+
+        }
+        console.log("🚀 ~ handleSave ~ val:", val)
+        AddDeleteUpdateData('AppUser/Update_AppUser', val).then((response) => {
+            console.log("🚀 ~ handleCheckBox ~ response:", response);
+            if (response?.success) {
+                showSuccess("Update Successfully")
+            }
+        })
+    };
+
+    const resetObject = () => {
+        setValue({
+            ...value,
+            'ButtonDetail': '',
+        })
+    }
+
+
     return (
         <div className="card border">
             <div className="card-body px-3 py-2">
@@ -20,6 +125,10 @@ export default function AdminInfo() {
                             type="text"
                             className="form-control form-control-sm"
                             placeholder="Enter the Username"
+                            name="UserName"
+                            value={value?.UserName}
+                            onChange={(e) => { setValue({ ...value, 'UserName': e.target.value }) }}
+
                         />
                     </div>
 
@@ -27,9 +136,12 @@ export default function AdminInfo() {
                     <div className="col-md-4">
                         <label className="small text-muted mb-1">Email</label>
                         <input
-                            type="email"
+                            type="text"
                             className="form-control form-control-sm"
                             placeholder="Enter the Email"
+                            name="EmailID"
+                            value={value?.EmailID}
+                            onChange={(e) => { setValue({ ...value, 'EmailID': e.target.value }) }}
                         />
                     </div>
 
@@ -37,9 +149,12 @@ export default function AdminInfo() {
                     <div className="col-md-4">
                         <label className="small text-muted mb-1">Password</label>
                         <input
-                            type="password"
+                            type="text"
                             className="form-control form-control-sm"
-                             placeholder="Enter the Password"
+                            placeholder="Enter the Password"
+                            name="Password"
+                            value={value?.Password}
+                            onChange={(e) => { setValue({ ...value, 'Password': e.target.value }) }}
                         />
                     </div>
 
@@ -47,7 +162,7 @@ export default function AdminInfo() {
 
                 {/* Button */}
                 <div className="mt-2">
-                    <button className="btn btn-success btn-sm">
+                    <button className="btn btn-success btn-sm" onClick={handleSave}>
                         UPDATE
                     </button>
                 </div>
