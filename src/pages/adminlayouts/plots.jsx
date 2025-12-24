eef9fea83d826e7035cf3cce04b2f28ed46ff98b
devArustu <devendra.arustu@gmail.com>
2025-12-24 10:09:23 +0530

    slv err

--- src/pages/adminlayouts/plots.jsx
+++ src/pages/adminlayouts/plots.jsx
@@ -60,7 +60,6 @@ const Plots = () => {
 
     });
 
-
     useEffect(() => {
         fetchApplications(CompanyID);
         getProjectNameDrpData(CompanyID);
@@ -337,7 +336,6 @@ const Plots = () => {
         });
         setPlotAreaUnit('Yard')
     }
-
 
     const customStyles = {
         headCells: {
