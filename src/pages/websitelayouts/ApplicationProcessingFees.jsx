da8bbeaf25d255f919b8deb1b541dea7c5f2a411
devArustu <devendra.arustu@gmail.com>
2026-01-06 16:13:56 +0530

    slv err

--- src/pages/websitelayouts/ApplicationProcessingFees.jsx
+++ src/pages/websitelayouts/ApplicationProcessingFees.jsx
@@ -137,7 +137,7 @@ const ApplicationProcessingFees = () => {
                                         }}
                                     />
                                 ))}
-                            </div>
+                            </div>  
 
                         </div>
 
